e304e38cc05c9418dfa2316b66f46a49f930e723
Sarah H <sarahhishammuddin@gmail.com>
2021-06-10 13:47:00 +0800

    checks if alumni is approved or not

--- processLogin.php
+++ processLogin.php
@@ -20,15 +20,21 @@ if($role == 'alumni'){
     while($row = mysqli_fetch_assoc($result)){
       $id = $row["userid"];
       $email = $row["email"];
-      session_start();
+      $status = $row['user_status'];
+      if($status == 'approved'){
+        session_start();
       $_SESSION['logged_in'] = true;
       $_SESSION['id'] = $id;
       $_SESSION['email'] = $email;
-    }
     header("Location: profile.php");
+    }
+    else{
+      header("Location: index.php? action=inactive");
+    }
   }
+}
   else{header("Location: login.php? action=login_failed");
-  }
+}
 }
 
 if($role == 'admin'){
