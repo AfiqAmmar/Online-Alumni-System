b394aa86e037e767cc0df5072be08a12c8f49dad
NUR AFIFAH BINTI AZMI <76671622+afifah1712@users.noreply.github.com>
2021-06-14 21:07:41 +0800

    Update Alumni.php

--- Alumni.php
+++ Alumni.php
@@ -1,5 +1,5 @@
 <?php 
-include('config.php'); 
+include('config\config.php'); 
 ?>
 <html lang="en">
 <head>
@@ -14,7 +14,7 @@ include('config.php');
     <title>FCSIT UM Alumni</title>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
     <?php
-      $_SESSION["email"]="afifah@gmail.com"
+     // $_SESSION["email"]="afifah@gmail.com"
     ?>
 </head>
 <body>  
